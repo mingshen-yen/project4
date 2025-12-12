df07b9589ac4797d7b400302adb8e2eb8a15ce63
mingshen-yen <minghsuanyen@gmail.com>
2025-12-12 16:12:30 +0100

    fetch movies

--- main.js
+++ main.js
@@ -1,19 +1,50 @@
-const url = "https://api.themoviedb.org/3/movie/popular?api_key=e88deaad2c5706752bff03d4decee143";
-const fetchMovie = () => {
+const apiToken = "e88deaad2c5706752bff03d4decee143";
+const page = 1;
+const apiURL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiToken}&page=${page}`;
+
+const fetchMovies = (url) => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.results); // array of movies
+      console.log(data); // array of movies
+      const Movies = data.results;
+      addToLocalStorage("Movies", data.results);
       data.results.forEach((movie) => {
-        console.log(movie.title);
-        console.log(movie.backdrop_path);
-        console.log(movie.release_date);
-        console.log(movie.original_language);
+        const movieList = {
+          title: movie.title,
+          imgURL: movie.poster_path,
+          date: movie.release_date,
+          language: movie.original_language,
+        };
+        addPersonalCard(movieList);
+        // console.log(movieList);
       });
     })
     .catch((error) => console.error("Error:", error));
 };
-fetchMovie();
-// fetch("https://api.themoviedb.org/3/movie/550?api_key=e88deaad2c5706752bff03d4decee143")
-//   .then((res) => res.json())
-//   .then((data) => console.log(data));
+
+const addToLocalStorage = (keyname, value) => {
+  localStorage.setItem(keyname, JSON.stringify(value));
+  console.log("Updated array:", value);
+};
+
+const addPersonalCard = (data) => {
+  const PersonalContainer = document.getElementById("personal-container");
+  PersonalContainer.innerHTML += `
+    <div class="card">
+      <img src="assets/imgs/favourite.png" class="icons hidden" id="liked"/>
+      <img src="https://image.tmdb.org/t/p/w500${data.imgURL}" alt="movie" class="movie_img" />
+      <h4 class='text-xl font-bold pt-2'>${data.title}</h4>
+      <span class= "thin text-base text-gray-400">${data.date}</span>
+      <span class= "thin text-sm text-gray-400" >${data.language}</span>
+    </div>`;
+};
+
+const selectLikes = () => {
+  const clickLikes = document.querySelector("#personal-container");
+  console.log(clickLikes);
+  // clickLikes.addEventListener("click", (event) => {});
+};
+
+fetchMovies(apiURL);
+selectLikes();
